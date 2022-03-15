1dba7046c04cbefad5bd650bcfb0680e058446db
Doga Gulhan <dcgulhan@gmail.com>
2022-03-15 11:16:00 +0100

    fix init for cleaned up files

--- musical/__init__.py
+++ musical/__init__.py
@@ -10,9 +10,8 @@ from .catalog import load_catalog
 from .preprocessing import remove_samples_based_on_gini, identify_distinct_cluster
 from .cluster import OptimalK
 from .simulation import simulate_LDA
-from .nmf_cov import NMFCOV
 from .nnls_sparse_exact import ExactSparseNNLS
-from .nnls_sparse2 import SparseNNLS
+from .nnls_sparse import SparseNNLS
 
 __all__ = ['beta_divergence',
            'sigplot_bar',
