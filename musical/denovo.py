318efb20eb8d5fffb8ccd317932a319387976a8e
Doga C. Gulhan <dcgulhan@gmail.com>
2022-03-24 10:51:44 +0100

    update denovo.py to account for reduced parameters in validate.py

--- musical/denovo.py
+++ musical/denovo.py
@@ -1126,37 +1126,44 @@ class DenovoSig:
         return self
 
     def validate_assignment(self, use_refit = False, clear_grid = False):
-        W_simul, H_simul, X_simul, best_grid_index, best_grid_index_sum, best_grid_indices, best_grid_indices_sum, error_W, error_H, dist_W, dist_max, dist_sum, dist_max_sig_index, dist_max_all, dist_sum_all, dist_max_sig_index_all, W_simul_all, H_simul_all, _, _, _, _ = validate(self)
 
-        self.W_simul = W_simul
-        self.H_simul = H_simul
-        self.X_simul = X_simul
+        W_simul_all, H_simul_all, X_simul_all, best_grid_index, best_grid_index_sum, best_grid_indices, best_grid_indices_sum, error_W_all, error_H_all, dist_W_all, dist_max_sig_index_all,  dist_max_all, dist_sum_all = validate(self)
+
         self.W_simul_all = W_simul_all
         self.H_simul_all = H_simul_all
+        self.X_simul_all = X_simul_all
         self.best_grid_index = best_grid_index
         self.best_grid_index_sum = best_grid_index_sum
         self.best_grid_indices = best_grid_indices
         self.best_grid_indices_sum = best_grid_indices_sum
-        self.error_W_simul = error_W
-        self.error_H_simul = error_H
-        self.dist_W_simul = dist_W
-        self.dist_max_simul = dist_max
-        self.dist_sum_simul = dist_sum
-        self.dist_max_simul_sig_index = dist_max_sig_index
-        self.dist_max_simul_all = dist_max_all
+        self.error_W_all = error_W_all
+        self.error_H_all = error_H_all
+        self.dist_W_all = dist_W_all
+        self.dist_max_all = dist_max_all
+        self.dist_sum_all = dist_sum_all
         self.dist_max_simul_sig_index_all = dist_max_sig_index_all
-
+        
+        # to use maximum distance only change best_grid_index_sum to best_grid_index
         if self.n_grid > 1:
             self.W_s = self.W_s_all[best_grid_index_sum]
             self.H_s = self.H_s_all[best_grid_index_sum]
-            self.signature_names = self.signature_names_all[best_grid_index_sum]
-            self.reconstruction_error_s = self.reconstruction_error_s_all[best_grid_index_sum]
-            self.set_params(thresh1 = [self.thresh1_all[best_grid_index_sum]],
-                            thresh2 = [self.thresh2_all[best_grid_index_sum]],
-                            thresh1_match = [self.thresh1_match_all[best_grid_index_sum]],
-                            thresh2_match = [self.thresh2_match_all[best_grid_index_sum]],
-                            thresh_new_sig = [self.thresh_new_sig_all[best_grid_index_sum]])
+        self.W_simul = self.W_simul_all[best_grid_index_sum]
+        self.H_simul = self.H_simul_all[best_grid_index_sum]
+        self.X_simul = self.X_simul_all[best_grid_index_sum]
+        self.dist_W_simul = self.dist_W_all[best_grid_index_sum]
+        self.error_W_simul = self.error_W_all[best_grid_index_sum]
+        self.error_H_simul = self.error_H_all[best_grid_index_sum]
+        self.signature_names = self.signature_names_all[best_grid_index_sum]
+        self.dist_max_simul = self.dist_max_all[best_grid_index_sum]
+        self.dist_sum_simul = self.dist_sum_all[best_grid_index_sum]
+        self.reconstruction_error_s = self.reconstruction_error_s_all[best_grid_index_sum]
+        self.set_params(thresh1 = [self.thresh1_all[best_grid_index_sum]],
+                        thresh2 = [self.thresh2_all[best_grid_index_sum]],
+                        thresh1_match = [self.thresh1_match_all[best_grid_index_sum]],
+                        thresh2_match = [self.thresh2_match_all[best_grid_index_sum]],
+                        thresh_new_sig = [self.thresh_new_sig_all[best_grid_index_sum]])
 
         if clear_grid:
             self.clear_grid()
         return self
+
