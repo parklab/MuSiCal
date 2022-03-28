63a0264086044119e96f0e211bdc58dc8262acc5
Hu Jin <jinhu.stanley@gmail.com>
2022-03-28 17:19:42 -0400

    Rewrite refit.py
    Use the following functions for matching and refitting
    1. refit, refit_grid
    2. match, match_grid
    3. assign, assign_grid
    The grid calculations use SparseNNLSGrid, and are thus nicely parallelized. In assign_grid, redundant matching results are also taken care of.

--- musical/refit.py
+++ musical/refit.py
@@ -1,11 +1,193 @@
-"""Main class for the refitting problem"""
+"""Refitting and matching
+
+TODO:
+1. Change the option indices_associated_sigs=None to something like connected_sigs=False.
+I.e., automatically set associated signatures. Or, at the same time allow user to specify their own
+association list. If they do not specify, use our default list. This change should be done after we revise
+nnls_sparse.py, where we replace indices_associated_sigs by names of associated sigs. That'll be easier to do.
+"""
+
 
 import numpy as np
 import scipy as sp
+import pandas as pd
 from .nnls import nnls
-from .nnls_sparse import SparseNNLS
+from .nnls_sparse import SparseNNLS, SparseNNLSGrid
 from .utils import match_signature_to_catalog_nnls_sparse, beta_divergence, get_sig_indices_associated
 from .catalog import load_catalog
+
+
+def refit(X, W, method='likelihood_bidirectional', thresh=None,
+          indices_associated_sigs=None):
+    """Wrapper around SparseNNLS for refitting
+
+    Note that only one parameter thresh1 is allowed here.
+    Both X and W should be pd.DataFrame.
+    """
+    # Check input
+    if X.shape[0] != W.shape[0]:
+        raise ValueError('X and W have different numbers of channels.')
+    if (X.index == W.index).sum() != X.shape[0]:
+        raise ValueError('X and W have different indices.')
+    # SparseNNLS
+    model = SparseNNLS(method=method, thresh1=thresh, indices_associated_sigs=indices_associated_sigs)
+    model.fit(X, W)
+    return model.H, model
+
+def refit_grid(X, W, method='likelihood_bidirectional', thresh_grid=None, ncpu=1, verbose=0,
+               indices_associated_sigs=None):
+    """Refitting on a grid of thresholds.
+    """
+    # Check input
+    if X.shape[0] != W.shape[0]:
+        raise ValueError('X and W have different numbers of channels.')
+    if (X.index == W.index).sum() != X.shape[0]:
+        raise ValueError('X and W have different indices.')
+    # SparseNNLSGrid
+    if thresh_grid is None:
+        thresh_grid = np.array([0.001])
+    model = SparseNNLSGrid(method=method, thresh1_grid=thresh_grid, ncpu=ncpu, verbose=verbose, indices_associated_sigs=indices_associated_sigs)
+    model.fit(X, W)
+    # Results
+    H_grid = {}
+    if model.thresh2_grid[0] is None:
+        for thresh in thresh_grid:
+            H_grid[thresh] = model.H_grid[(thresh, None)]
+    elif model.thresh2_grid[0] == 0.0:
+        for thresh in thresh_grid:
+            H_grid[thresh] = model.H_grid[(thresh, 0.0)]
+    else:
+        raise ValueError('thresh2 is modified unexpectedly.')
+    return H_grid, model
+
+def _get_W_s(W, W_catalog, H_reduced, cos_similarities, thresh_new_sig):
+    """Auxiliary function for matching"""
+    inds_new_sig = np.arange(0, W.shape[1])[np.array(cos_similarities) < thresh_new_sig]
+    inds_not_new_sig = np.arange(0, W.shape[1])[np.array(cos_similarities) >= thresh_new_sig]
+    if len(inds_new_sig) > 0 and len(inds_not_new_sig) > 0:
+        W_s = pd.DataFrame.copy(W.iloc[:, inds_new_sig])
+        # Let's not rename these new signatures and keep their original name in W, so that we can distinguish them.
+        #W_s.columns = ['Sig_N' + str(i) for i in range(1, len(inds_new_sig) + 1)]
+        H_tmp = H_reduced.iloc[:, inds_not_new_sig]
+        W_s = pd.concat([W_s, W_catalog[H_tmp.index[H_tmp.sum(1) > 0]]], axis=1)
+    elif len(inds_new_sig) > 0 and len(inds_not_new_sig) == 0:
+        W_s = pd.DataFrame.copy(W.iloc[:, inds_new_sig])
+    elif len(inds_new_sig) == 0 and len(inds_not_new_sig) > 0:
+        W_s = pd.DataFrame.copy(W_catalog[H_reduced.index])
+    return W_s
+
+def match(W, W_catalog, thresh_new_sig=0.8, method='likelihood_bidirectional', thresh=None,
+          indices_associated_sigs=None):
+    """Wrapper around SparseNNLS for matching
+
+    Note that only one parameter thresh1 is allowed here.
+    Both W and W_catalog should be pd.DataFrame.
+
+    TODO:
+    1. Directly using indices_associated_sigs does not make sense for matching.
+    We should do this outside of SparseNNLS, i.e., after SparseNNLS, we check the
+    set of matched signatures, and include additional associated signatures when necessary.
+    But for now, this is okay. It does not affect the final set of matched signatures.
+    """
+    # Check input
+    if W.shape[0] != W_catalog.shape[0]:
+        raise ValueError('W and W_catalog have different numbers of channels.')
+    if (W.index == W_catalog.index).sum() != W.shape[0]:
+        raise ValueError('W and W_catalog have different indices.')
+    if len(set(W.columns).intersection(W_catalog.columns)) > 0:
+        raise ValueError('W and W_catalog cannot contain signatures with the same name.')
+    # SparseNNLS
+    model = SparseNNLS(method=method, thresh1=thresh, indices_associated_sigs=indices_associated_sigs)
+    model.fit(W, W_catalog)
+    # Identify new signatures not in the catalog.
+    W_s = _get_W_s(W, W_catalog, model.H_reduced, model.cos_similarities, thresh_new_sig)
+    return W_s, model
+
+def match_grid(W, W_catalog, thresh_new_sig=0.8, method='likelihood_bidirectional', thresh_grid=None, ncpu=1, verbose=0,
+               indices_associated_sigs=None):
+    """Matching on a grid of thresholds.
+    """
+    # Check input
+    if W.shape[0] != W_catalog.shape[0]:
+        raise ValueError('W and W_catalog have different numbers of channels.')
+    if (W.index == W_catalog.index).sum() != W.shape[0]:
+        raise ValueError('W and W_catalog have different indices.')
+    if len(set(W.columns).intersection(W_catalog.columns)) > 0:
+        raise ValueError('W and W_catalog cannot contain signatures with the same name.')
+    # SparseNNLSGrid
+    if thresh_grid is None:
+        thresh_grid = np.array([0.001])
+    model = SparseNNLSGrid(method=method, thresh1_grid=thresh_grid, ncpu=ncpu, verbose=verbose, indices_associated_sigs=indices_associated_sigs)
+    model.fit(W, W_catalog)
+    # Identify new signatures not in the catalog.
+    thresh2 = model.thresh2_grid[0]
+    if thresh2 is None:
+        pass
+    elif thresh2 == 0.0:
+        pass
+    else:
+        raise ValueError('thresh2 is modified unexpectedly.')
+    W_s_grid = {}
+    for thresh in thresh_grid:
+        key = (thresh, thresh2)
+        W_s = _get_W_s(W, W_catalog, model.H_reduced_grid[key], model.cos_similarities_grid[key], thresh_new_sig)
+        W_s_grid[thresh] = W_s
+    return W_s_grid, model
+
+def assign(X, W, W_catalog=None,
+           method='likelihood_bidirectional',
+           thresh_match=None,
+           thresh_refit=None,
+           thresh_new_sig=0.8,
+           indices_associated_sigs=None):
+    """Assign = match + refit.
+
+    The same method will be used for both match and refit.
+    Match and refit can have different thresholds. But only one threshold is allowed for each.
+    If you want to skip matching, set thresh_new_sig to a value > 1.
+    """
+    W_s, _ = match(W, W_catalog, thresh_new_sig=thresh_new_sig, method=method, thresh=thresh_match, indices_associated_sigs=indices_associated_sigs)
+    H_s, _ = refit(X, W_s, method=method, thresh=thresh_refit, indices_associated_sigs=indices_associated_sigs)
+    return W_s, H_s
+
+def assign_grid(X, W, W_catalog, method='likelihood_bidirectional',
+                thresh_match_grid=None, thresh_refit_grid=None,
+                thresh_new_sig=0.8, indices_associated_sigs=None,
+                ncpu=1, verbose=0):
+    """Match and refit on a grid"""
+    if thresh_match_grid is None:
+        thresh_match_grid = np.array([0.001])
+    if thresh_refit_grid is None:
+        thresh_refit_grid = np.array([0.001])
+    # First, matching on a grid
+    W_s_grid_1d, _ = match_grid(W, W_catalog, thresh_new_sig=thresh_new_sig, method=method,
+                                thresh_grid=thresh_match_grid, ncpu=ncpu, verbose=verbose,
+                                indices_associated_sigs=indices_associated_sigs)
+    # Second, refitting on a grid.
+    # When a matching result is already calculated before, do not do refitting again.
+    H_s_grid = {}
+    calculated_matching_results = {}
+    thresh_match_grid_unique = []
+    for thresh_match in thresh_match_grid:
+        sigs = tuple(sorted(W_s_grid_1d[thresh_match].columns.tolist()))
+        if sigs in calculated_matching_results.keys():
+            H_s_grid[thresh_match] = H_s_grid[calculated_matching_results[sigs]]
+        else:
+            H_s_grid_1d, _ = refit_grid(X, W_s_grid_1d[thresh_match],
+                                        method=method, thresh_grid=thresh_refit_grid, ncpu=ncpu, verbose=verbose,
+                                        indices_associated_sigs=indices_associated_sigs)
+            H_s_grid[thresh_match] = H_s_grid_1d
+            calculated_matching_results[sigs] = thresh_match
+            thresh_match_grid_unique.append(thresh_match)
+    return W_s_grid_1d, H_s_grid, np.array(thresh_match_grid_unique)
+
+
+
+
+
+###############################################################################
+################################ Old codes below ##############################
+###############################################################################
 
 def refit_matrix(X, W, method = 'likelihood_bidirectional',
                  thresh1 = 0.001,
@@ -33,7 +215,7 @@ def refit_matrix(X, W, method = 'likelihood_bidirectional',
            H.append(h)
        H = np.array(H)
        X_reconstructed = np.array([W.values @ h for h in H.T.values]).T
-       
+
    else:
        sparse_method = SparseNNLS(method = method,
                                   thresh1 = thresh1,
@@ -45,7 +227,7 @@ def refit_matrix(X, W, method = 'likelihood_bidirectional',
    reconstruction_error = beta_divergence(X, np.array(X_reconstructed), beta=1, square_root=False)
 
    H = np.array(H)
-   
+
    return H, reconstruction_error
 
 def param_search_same_length(v1, v2, v3 = None):
@@ -93,8 +275,6 @@ def param_search_same_length(v1, v2, v3 = None):
     else:
         return v1, v2, v3, n_params
 
-
-
 def get_decomposed_W(W, W_catalog, signatures, thresh1, thresh2, thresh_new_sig):
     inds = [] # indices of the catalog to which W model matches to
     inds_w_model_new_sig = [] # indices of W from the model that are not matched to the catalog
@@ -136,15 +316,15 @@ def reassign(model, W_catalog, signatures, force_assign_associated = False):  # 
 
     # We need to write here an if to give error if parameters are not set
 
-    use_catalog = model.use_catalog 
-    thresh1_match = model.thresh1_match 
-    thresh2_match = model.thresh2_match 
-    method_sparse = model.method_sparse 
+    use_catalog = model.use_catalog
+    thresh1_match = model.thresh1_match
+    thresh2_match = model.thresh2_match
+    method_sparse = model.method_sparse
     thresh1 = model.thresh1
-    thresh2 = model.thresh2 
-    thresh1_match = model.thresh1_match 
-    thresh2_match = model.thresh2_match 
-    thresh_new_sig = model.thresh_new_sig 
+    thresh2 = model.thresh2
+    thresh1_match = model.thresh1_match
+    thresh2_match = model.thresh2_match
+    thresh_new_sig = model.thresh_new_sig
 
     """
     Example for running without denovo calculation
@@ -164,7 +344,7 @@ def reassign(model, W_catalog, signatures, force_assign_associated = False):  # 
         if False : Signatures discovered de-novo are used
         if True : First signatures are matched to catalog and a new matrix of signatures
         are calculated using match_signature_to_catalog function
-    
+
     thresh1_match, thresh2_match: Used to define the SparseNNLS object to match denovo signature to the catalog
     thresh_new_sig : 1-d numpy array
         If the cosine of the composite signature is smaller than this value the signature is considered
@@ -186,7 +366,7 @@ def reassign(model, W_catalog, signatures, force_assign_associated = False):  # 
     thresh1, thresh2, n_params_sparse = param_search_same_length(thresh1, thresh2)
     thresh1_match, thresh2_match, thresh_new_sig, n_params_match = param_search_same_length(thresh1_match, thresh2_match, thresh_new_sig)
 
-    
+
     W = model.W
 
     X = model.X
@@ -218,7 +398,7 @@ def reassign(model, W_catalog, signatures, force_assign_associated = False):  # 
             for j in np.arange(0, n_params_match): # loop over parameters used in catalog match
                 W_s_this, signames_this = get_decomposed_W(W, W_catalog, signatures, thresh1_match[j], thresh2_match[j], thresh_new_sig[j])
 
-                if force_assign_associated: 
+                if force_assign_associated:
                     indices_associated_sigs, signames_this_tmp = get_sig_indices_associated(signames_this, signatures)
                     signames_this_tmp = np.array(signames_this_tmp)
                     if signames_this.size != signames_this_tmp.size:
@@ -236,7 +416,7 @@ def reassign(model, W_catalog, signatures, force_assign_associated = False):  # 
                                                     thresh1 = thresh1[i],
                                                     thresh2 = thresh2[i],
                                                     indices_associated_sigs = indices_associated_sigs )
-                
+
                 # a flat paramater array following the same indices as W_s and H_s are generated
                 # to be used in validation step to pick the best parameters once the best W_s and H_s are determined
                 thresh1_all = np.append(thresh1_all, thresh1[i])
@@ -275,5 +455,5 @@ def reassign(model, W_catalog, signatures, force_assign_associated = False):  # 
         signames[i] = signames[i][ np.sum(H_s[i], axis = 1) > 0]
         H_s[i] = H_s[i][np.sum(H_s[i], axis = 1) > 0, :]
 
-    
+
     return W_s, H_s, signames, reconstruction_error_s_all, n_grid, thresh1_all, thresh2_all, thresh1_match_all, thresh2_match_all, thresh_new_sig_all
