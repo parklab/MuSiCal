cd642a6872eebae9ccdb03259669a67db5d2eb6f
Hu Jin <jinhu.stanley@gmail.com>
2022-04-02 23:57:45 -0400

    Update get_sig_indices_associated
    Do not sort signatures. So that the returned signatures are the same as signatures, if signatures is complete, i.e., contains all connected signatures.

--- musical/utils.py
+++ musical/utils.py
@@ -663,7 +663,6 @@ def get_sig_indices_associated(signatures, signatures_catalog=None):
         if has_entry and len(missing_item) > 0:
             signatures = np.append(signatures, missing_item)
 
-    signatures = np.sort(signatures)
     if signatures_catalog is not None:
         signatures = [item for index,item in enumerate(signatures_catalog) if item in signatures]
         signatures = np.array(signatures)
