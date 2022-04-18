4943f5604ed5dd20c47d6157da679ddf45dd64aa
Hu Jin <jinhu.stanley@gmail.com>
2022-04-18 13:38:35 -0400

    Change name of clear_W_s to clean_W_s

--- musical/denovo.py
+++ musical/denovo.py
@@ -984,7 +984,7 @@ class DenovoSig:
             plt.savefig(outfile, bbox_inches='tight')
 
     def assign(self, W_catalog, method_assign='likelihood_bidirectional',
-               thresh_match=None, thresh_refit=None, thresh_new_sig=0.8, connected_sigs=False, clear_W_s=False):
+               thresh_match=None, thresh_refit=None, thresh_new_sig=0.8, connected_sigs=False, clean_W_s=False):
         # Check if fit has been run
         if not hasattr(self, 'W'):
             raise ValueError('The model has not been fitted.')
@@ -1014,13 +1014,13 @@ class DenovoSig:
         self.thresh_new_sig = thresh_new_sig
         self.connected_sigs = connected_sigs
         if self.W.shape[0] == 96:
-            if not clear_W_s:
-                warnings.warn('For 96-dimensional SBS signatures, you can try clear_W_s=True.',
+            if not clean_W_s:
+                warnings.warn('For 96-dimensional SBS signatures, you can try clean_W_s=True.',
                               UserWarning)
-        self.clear_W_s = clear_W_s
+        self.clean_W_s = clean_W_s
         self.W_s, self.H_s, self.sig_map = assign(self.X_df, self.W_df, self.W_catalog, method=self.method_assign,
                                                   thresh_match=self.thresh_match, thresh_refit=self.thresh_refit, thresh_new_sig=self.thresh_new_sig,
-                                                  connected_sigs=self.connected_sigs, clear_W_s=self.clear_W_s)
+                                                  connected_sigs=self.connected_sigs, clean_W_s=self.clean_W_s)
         self.sigs_assigned = self.H_s.index[self.H_s.sum(1) > 0].values
         self.n_sigs_assigned = len(self.sigs_assigned)
         self.W_s = pd.DataFrame.copy(self.W_s[self.sigs_assigned])
@@ -1029,7 +1029,7 @@ class DenovoSig:
         return self
 
     def assign_grid(self, W_catalog, method_assign='likelihood_bidirectional',
-                    thresh_match_grid=None, thresh_refit_grid=None, thresh_new_sig=0.8, connected_sigs=False, clear_W_s=False):
+                    thresh_match_grid=None, thresh_refit_grid=None, thresh_new_sig=0.8, connected_sigs=False, clean_W_s=False):
         # Check if fit has been run
         if not hasattr(self, 'W'):
             raise ValueError('The model has not been fitted.')
@@ -1059,16 +1059,16 @@ class DenovoSig:
         self.thresh_new_sig = thresh_new_sig
         self.connected_sigs = connected_sigs
         if self.W.shape[0] == 96:
-            if not clear_W_s:
-                warnings.warn('For 96-dimensional SBS signatures, you can try clear_W_s=True.',
+            if not clean_W_s:
+                warnings.warn('For 96-dimensional SBS signatures, you can try clean_W_s=True.',
                               UserWarning)
-        self.clear_W_s = clear_W_s
+        self.clean_W_s = clean_W_s
         W_s_grid_1d, H_s_grid, sig_map_grid_1d, thresh_match_grid_unique = assign_grid(
             self.X_df, self.W_df, self.W_catalog, method=self.method_assign,
             thresh_match_grid=self.thresh_match_grid, thresh_refit_grid=self.thresh_refit_grid,
             thresh_new_sig=self.thresh_new_sig,
             connected_sigs=self.connected_sigs,
-            clear_W_s=self.clear_W_s,
+            clean_W_s=self.clean_W_s,
             ncpu=self.ncpu, verbose=self.verbose
         )
         self.sig_map_grid_1d = sig_map_grid_1d
